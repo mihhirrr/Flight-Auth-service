24ede14c9b68a87df58a995564f0694f72e4195b
Mihir D <mihirdongare1045@gmail.com>
2025-12-19 18:12:42 +0530

    FIX: Corrected syntax for declaration of Name attribute as ENUM in models/profiles.js

--- src/models/profile.js
+++ src/models/profile.js
@@ -8,23 +8,21 @@ const { ADMIN, CUSTOMER } = Enums.User_Profile;
 
 module.exports = (sequelize, DataTypes) => {
   class Profile extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      this.belongsToMany(models.user, {
+      this.belongsToMany(models.User, {
         through: 'user_profiles',
         foreignKey: 'ProfileId',
       });
     }
   }
   Profile.init({
-    Name: DataTypes.ENUM,
-    values: [ ADMIN, CUSTOMER ], 
-    allowNull: false,
-    default: CUSTOMER
+    Name: {
+      type: DataTypes.ENUM,
+      values: [ ADMIN, CUSTOMER ],
+      allowNull: false,
+      default: CUSTOMER
+    }, 
   }, 
   {
     sequelize,
